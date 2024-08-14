28198be86763742f25365c6a57df268249f419ca
DonutSergius <popov.serg.2003.09@gmail.com>
2024-08-14 17:43:49 +0300

    Added Static Block on page

---
+++ src/blocks/StaticBlock.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+class StaticBlock extends React.Component {
+
+    render () {
+        const {
+            blockTitle,
+            blockImageUrl,
+            blockImageAlt,
+            blockBody,
+            blockLink,
+            blockLinkUrl,
+        } = this.props;
+
+        return (
+            <div className="static-block-container row">
+                <div className="static-block-data col-lg-6 col-md-12">
+                    <div className="static-block-data_title">
+                        {blockTitle}
+                    </div>
+                    <div className="static-block-data_body">
+                        {blockBody}
+                    </div>
+                    <div className="static-block-data_read-more">
+                        <a
+                            className="read-more"
+                            href={blockLinkUrl}
+                        >
+                            {blockLink}
+                        </a>
+                    </div>
+                </div>
+                <div className="static-block-image col-lg-6 col-md-12">
+                    <img src={blockImageUrl} alt={blockImageAlt}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default StaticBlock;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,19 +3,32 @@ import Service from "../services/Service";
 import Header from "../components/Header";
 import FeatureArticle from "../blocks/FeatureArticle";
 import TutorialDesign from "../blocks/TutorialDesign";
+import StaticBlock from "../blocks/StaticBlock";
 
 class Blog extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
             title: '',
+            blogStaticBlock: [],
+            blogStaticBlockBody: '',
+            blogStaticBlockImageUrl: '',
         };
     }
 
     async componentDidMount() {
-        const rawHtml = await Service.getTitleLogo(); // Отримання асинхронних даних
+        const rawHtml = await Service.getTitleLogo();
         const title = this.stripHtmlTags(rawHtml);
-        this.setState({ title });
+
+        const blockData = await Service.getBlogStaticBlock();
+        const blockDataBody = this.stripHtmlTags(blockData.body.processed);
+        const blockDataImageUrl = await Service.getImage(blockData.field_image.uri.url);
+        this.setState({
+            title: title,
+            blogStaticBlock: blockData,
+            blogStaticBlockBody: blockDataBody,
+            blogStaticBlockImageUrl: blockDataImageUrl,
+        });
     }
 
     stripHtmlTags(text) {
@@ -23,7 +36,11 @@ class Blog extends React.Component{
     }
 
     render () {
-        const { title } = this.state;
+        const { title,
+            blogStaticBlock,
+            blogStaticBlockBody,
+            blogStaticBlockImageUrl,
+        } = this.state;
         return (
             <div className="blog-container">
                 <div className="dark">
@@ -33,6 +50,18 @@ class Blog extends React.Component{
                 </div>
                 <div className="whitearea">
                     <TutorialDesign />
+                </div>
+                <div className="dark">
+                    {blogStaticBlock && (
+                        <StaticBlock
+                            blockTitle={blogStaticBlock.field_title}
+                            blockImageUrl={blogStaticBlockImageUrl}
+                            blockImageAlt={blogStaticBlock.field_image && blogStaticBlock.field_image.meta ? blogStaticBlock.field_image.meta.alt : ''}
+                            blockBody={blogStaticBlockBody}
+                            blockLink={blogStaticBlock.field_link && blogStaticBlock.field_link.title}
+                            blockLinkUrl={blogStaticBlock.field_link && blogStaticBlock.field_link.uri}
+                        />
+                    )}
                 </div>
             </div>
         );
